6f79be82612b09f5792a515b16ce2b9b1d4b2a25
Adrian Zwiener <zwieneradrian@web.de>
2016-05-30 10:01:14 +0200

    not working function

--- jaco2_calibration/include/jaco2_calibration/jaco2_calibration_io.hpp
+++ jaco2_calibration/include/jaco2_calibration/jaco2_calibration_io.hpp
@@ -1,6 +1,7 @@
 #ifndef JACO2_CALIBRATION_IO_HPP
 #define JACO2_CALIBRATION_IO_HPP
 #include <string>
+#include <stdio.h>
 #include <vector>
 #include <fstream>
 #include <jaco2_calibration/dynamic_calibrated_parameters.hpp>
@@ -54,6 +55,67 @@ void save(std::string name, std::vector<DynamicCalibrationSample> samples, std::
     for(DynamicCalibrationSample sample : samples)
     {
         file << sample.toString(delimiter) << std::endl;
+    }
+}
+
+void importAsciiData(std::string filename, std::vector<DynamicCalibrationSample>& samples, std::string delimiter = std::string(";"))
+{
+    samples.clear();
+
+    std::string line;
+    std::ifstream infile;
+    double ts, d[24];
+    double start = 0.0;
+
+    infile.open ( filename );
+    if ( infile.is_open() )
+    {
+      char format[266];
+//      switch ( type )
+//      {
+//      case DATASET_COMMA_SEPARATED:
+////          sprintf_s ( format,"%%lf, %%lf, %%lf, %%lf" );
+//          sprintf ( format,"%%lf, %%lf, %%lf, %%lf" );
+//        break;
+//      case DATASET_SPACE_SEPARATED:
+//      default:
+////          sprintf_s ( format,"%%lf %%lf %%lf %%lf" );
+//          sprintf ( format,"%%lf %%lf %%lf %%lf" );
+//        break;
+//      }
+
+      int l = 0;
+      while ( std::getline ( infile,line ) )
+      {
+//          int res = sscanf_s ( line.data(), format, &ts, &d[0], &d[1], &d[2] );
+          int res = sscanf ( line.data(), format, &ts, &d[0], &d[1], &d[2], &d[3], &d[4], &d[5],        // pos
+                                                       &d[6], &d[7], &d[8], &d[9], &d[10], &d[11],      // vel
+                                                       &d[12], &d[13], &d[14], &d[15], &d[16], &d[17],  // acc
+                                                       &d[18], &d[19], &d[20], &d[21], &d[22], &d[23]);
+        if ( res != 4 )
+        {
+          std::cout<<"importAsciiData(): error importing data in line "<<l<<", exit"<< std::endl;
+        }
+        else
+        {
+            if(start == 0.0) {
+                start = ts;
+            }
+
+            ts -= start;
+//          ts /= unit;
+          DynamicCalibrationSample sample;
+          sample.time = ts;
+          sample.jointPos = {d[0], d[1], d[2], d[3], d[4], d[5]};
+          sample.jointVel = {d[6], d[7], d[8], d[9], d[10], d[11]};
+          sample.jointAcc = {d[12], d[13], d[14], d[15], d[16], d[17]};
+          sample.jointTorque = {d[18], d[19], d[20], d[21], d[22], d[23]};
+          samples.push_back(sample);
+//            std::cout <<  d[0]<<", "<< d[1]<<", " <<d[2]<<std::endl;
+        }
+        l++;
+      }
+      infile.close();
     }
 }
 
