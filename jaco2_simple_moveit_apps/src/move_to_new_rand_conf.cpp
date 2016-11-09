5259d4df8aaca8a09572b4b48a7ce68b2f739b6b
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2016-11-09 09:56:33 +0100

    implemented comparation in move_to_new_rand_conf

--- jaco2_simple_moveit_apps/src/move_to_new_rand_conf.cpp
+++ jaco2_simple_moveit_apps/src/move_to_new_rand_conf.cpp
@@ -6,19 +6,68 @@
 #include <moveit_msgs/DisplayTrajectory.h>
 
 struct Configuration{
+
+    bool equal(Configuration& other, std::vector<double>& threshold)
+    {
+        if(other.angles.size() == angles.size()){
+            bool eq = true;
+            for(std::size_t i = 0; i < angles.size(); ++i){
+                double diff = std::abs(angles[i] - other.angles[i]);
+                eq &= diff < threshold;
+            }
+            return eq;
+        }
+        else{
+            return false;
+        }
+    }
+
+    std::string to_string()
+    {
+        std::string res;
+        for(auto val : angles){
+            res += std::to_string(val) + ";";
+        }
+    }
+
     std::vector<double> angles;
 };
 
 struct ConfigurationList{
 
-    bool contains(Configurations)
+    bool contains(Configurations& conf)
+    {
+        if(conf.angles.size() == jointNames.size()){
+            bool test = false;
+            for(auto elemets : configurations){
+                test |= elemets.equal(conf, offsets);
+                if(test){
+                    return test;
+                }
+            }
+            else return test;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void save(std::string filename)
+    {
+          std::ofstream file(filename);
+
+          for()
+    }
+
+    void load(std::string filename)
     {
 
     }
 
+    std::size_t n_joints_;
     std::vector<double> offsets;
     std::vector<std::string> jointNames;
-    std::vector<std::vector<double>> Configurations;
+    std::vector<Configuration> configurations;
 };
 
 class NewRandomConfPlanner{
@@ -76,30 +125,30 @@ public:
                 collision = true;
             }
 
-    }
+        }
 
-private:
+        private:
 
-    moveit::planning_interface::MoveGroup group_;
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+        moveit::planning_interface::MoveGroup group_;
+        moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
 
-    planning_scene_monitor::PlanningSceneMonitorPtr planningMonitor_;
+        planning_scene_monitor::PlanningSceneMonitorPtr planningMonitor_;
 
-    ConfigurationList configurations_;
-};
+        ConfigurationList configurations_;
+    };
 
-int main(int argc, char *argv[])
-{
-    ros::init (argc, argv, "home_arm");
-    ros::NodeHandle node_handle("~");
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
+    int main(int argc, char *argv[])
+    {
+        ros::init (argc, argv, "home_arm");
+        ros::NodeHandle node_handle("~");
+        ros::AsyncSpinner spinner(1);
+        spinner.start();
 
-    ros::Duration sleep_time(1.0);
-    sleep_time.sleep();
-    sleep_time.sleep();
+        ros::Duration sleep_time(1.0);
+        sleep_time.sleep();
+        sleep_time.sleep();
 
-    std::vector<double> offsets;
+        std::vector<double> offsets;
 
 
 
