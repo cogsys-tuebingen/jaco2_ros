b6f1c9fcd52f3f530cf56d69b4a23ebd4cc562b9
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2017-01-10 13:57:08 +0100

    distFunction calls for variable configurations

--- jaco2_simple_moveit_apps/src/distFunction.cpp
+++ jaco2_simple_moveit_apps/src/distFunction.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
     ros::NodeHandle node_handle("~");
     ros::AsyncSpinner spinner(1);
     spinner.start();
-//    sleep(4.0);
+    //    sleep(4.0);
 
     moveit::planning_interface::MoveGroup group_("manipulator");
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
@@ -26,9 +26,9 @@ int main(int argc, char** argv) {
 
     group_.setStartStateToCurrentState();
 
-//###############################################################################################################################
-//###############################################################################################################################
-//    saftyBox();
+    //###############################################################################################################################
+    //###############################################################################################################################
+    //    saftyBox();
     double z = -0.2; //formerly 0
     double xm = -1;
     double xp = 1;
@@ -65,11 +65,10 @@ int main(int argc, char** argv) {
 
     collision_objects.push_back(collision_object);
     planning_scene_interface_.addCollisionObjects(collision_objects);
-//###############################################################################################################################
-//###############################################################################################################################
+    //###############################################################################################################################
+    //###############################################################################################################################
 
     //move_new_rand_conf_call
-
     planning_scene_monitor::LockedPlanningSceneRW ps(planningMonitor_);
     //    robot_state::RobotState state = ps->getCurrentStateNonConst();
     ps->getCurrentStateNonConst().update();
@@ -87,29 +86,45 @@ int main(int argc, char** argv) {
     collision_result.clear();
 
     robot_state::RobotState state = scene->getCurrentStateNonConst();
-//    robot_state::RobotState& state = planningMonitor_->getPlanningScene()->getCurrentStateNonConst();
+    //    robot_state::RobotState& state = planningMonitor_->getPlanningScene()->getCurrentStateNonConst();
     state.update();
     state.updateCollisionBodyTransforms();
+    std::vector<double> q = {0,M_PI,M_PI,0,0,0};
+    std::vector<std::string> names = planningMonitor_->getRobotModel()->getJointModelGroup("manipulator")->getActiveJointModelNames();
 
-    scene->setCurrentState(state);
-    scene->checkCollision(collision_request, collision_result, state, acm);
-    ROS_INFO_STREAM("result.distance (current conf) " << collision_result.distance);
+    for(int i = 0; i < 2; ++i)
+    {
+        if(i==1){
+//            state.setToRandomPositions(); // works as well
+            auto it_q = q.begin();
+            for(auto lname : names){
+                state.setVariablePosition(lname, *it_q);
+                ++it_q;
+            }
+//            state.update();
+        }
+
+            for(auto lname : names){
+                ROS_INFO_STREAM(lname << " position " << *state.getJointPositions(lname));
+            }
+        scene->setCurrentState(state);
 
 
-    std::map<std::string, moveit_msgs::CollisionObject> c_objects_map = planning_scene_interface_.getObjects();
-    for(auto& kv : c_objects_map){
-        ROS_INFO_STREAM( kv.first << " has value " << kv.second );
-        scene->processCollisionObjectMsg(kv.second);
+        std::map<std::string, moveit_msgs::CollisionObject> c_objects_map = planning_scene_interface_.getObjects();
+        for(auto& kv : c_objects_map){
+//            ROS_INFO_STREAM( kv.first << " has value " << kv.second );
+            scene->processCollisionObjectMsg(kv.second);
+        }
+
+        //    for(auto i : collision_objects)
+        //        scene->processCollisionObjectMsg(i);
+
+
+
+
+        double distance_distanceToCollision = scene->distanceToCollision(state);
+        ROS_INFO_STREAM("distanceToCollision(current conf "<< i << "): " << distance_distanceToCollision);
     }
-
-//    for(auto i : collision_objects)
-//        scene->processCollisionObjectMsg(i);
-
-
-
-
-    double distance_distanceToCollision = scene->distanceToCollision(state);
-    ROS_INFO_STREAM("distanceToCollision(current conf): " << distance_distanceToCollision);
 
     double distance_distanceRobot =  scene->getCollisionWorld()->distanceRobot(*(scene->getCollisionRobot()), state, scene->getAllowedCollisionMatrix());
     ROS_INFO_STREAM("calling distancerobot (current conf) : "<< distance_distanceRobot);
