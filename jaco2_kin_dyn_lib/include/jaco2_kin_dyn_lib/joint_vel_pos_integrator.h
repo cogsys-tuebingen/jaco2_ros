505c1c35effb3df40af89ff2332254dfdfd093b7
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2017-05-31 13:58:19 +0200

    commit include all heades; integrator class

---
+++ jaco2_kin_dyn_lib/include/jaco2_kin_dyn_lib/joint_vel_pos_integrator.h
@@ -0,0 +1,10 @@
+#ifndef JOINT_VEL_POS_INTEGRATOR_H
+#define JOINT_VEL_POS_INTEGRATOR_H
+#include <jaco2_kin_dyn_lib/jaco2_dynamic_model.h>
+
+class JointVelPosIntegrator
+{
+public:
+    JointVelPosIntegrator() {}
+};
+#endif // JOINT_VEL_POS_INTEGRATOR_H
