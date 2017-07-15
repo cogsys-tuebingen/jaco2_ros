72fb24ec0f27ea487ac7a58e55f5ec2a9a8993bd
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2017-07-15 23:45:09 +0200

    vector3stamped arithmetic

--- jaco2_data/include/jaco2_data/vector3stamped.h
+++ jaco2_data/include/jaco2_data/vector3stamped.h
@@ -10,6 +10,33 @@ class Vector3Stamped
 public:
     Vector3Stamped();
 
+    Vector3Stamped(double val);
+
+    Vector3Stamped(double x, double y, double z);
+    Vector3Stamped operator+(const Vector3Stamped &other) const;
+    Vector3Stamped operator-(const Vector3Stamped &other) const;
+
+    Vector3Stamped& operator+=(const Vector3Stamped &other);
+    Vector3Stamped& operator-=(const Vector3Stamped &other);
+    Vector3Stamped operator*(const double &b) const;
+
+    Vector3Stamped abs() const;
+
+    ///
+    /// \brief operator * element wise multiplication
+    /// \param other the other vector
+    /// \return element wise product
+    ///
+    Vector3Stamped operator*(const Vector3Stamped &other) const;
+
+    Vector3Stamped operator/(const double &b) const;
+
+    Vector3Stamped operator*=(const double &b);
+    Vector3Stamped operator/=(const double &b);
+
+    double norm() const;
+    std::vector<double> toVector() const;
+
 public:
     TimeStamp stamp;
     std::string frame_id;
