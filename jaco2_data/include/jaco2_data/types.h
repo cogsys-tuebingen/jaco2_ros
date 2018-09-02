7ea941e64165a2c8759faecf3f3089436fd3f9c9
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2018-09-02 16:26:52 +0200

    Change some includes

--- jaco2_data/include/jaco2_data/types.h
+++ jaco2_data/include/jaco2_data/types.h
@@ -1,7 +1,8 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#include <Eigen/StdVector>
+#include <vector>
+#include <Eigen/Core>
 #include <jaco2_data/stamped_data.h>
 
 // FORWARD DECLARATIONS
@@ -36,34 +37,22 @@ typedef StampedData<ExtendedJointStateData> ExtendedJointStateDataStamped;
 typedef StampedData<JointData> JointDataStamped;
 typedef StampedData<JointAngles> JointAnglesStamped;
 
-typedef std::vector<Vector3,
-Eigen::aligned_allocator<Eigen::Vector3d> > Vector3Collection;
-typedef std::vector<Vector3Stamped,
-Eigen::aligned_allocator<Eigen::Vector3d> > Vector3StampedCollection;
+typedef std::vector<Vector3, EV3dAllocator> Vector3Collection;
+typedef std::vector<Vector3Stamped, EV3dAllocator> Vector3StampedCollection;
 
-typedef std::vector<JointStateData,
-Eigen::aligned_allocator<Eigen::Vector3d> >
-JointStateDataCollection;
+typedef std::vector<JointStateData, EV3dAllocator> JointStateDataCollection;
 
-typedef std::vector<JointStateDataStamped,
-Eigen::aligned_allocator<Eigen::Vector3d> >
-JointStateDataStampedCollection;
+typedef std::vector<JointStateDataStamped, EV3dAllocator> JointStateDataStampedCollection;
 
-typedef std::deque<JointStateData,
-Eigen::aligned_allocator<Eigen::Vector3d> > JointStateDataDeque;
-typedef std::deque<JointStateDataStamped,
-Eigen::aligned_allocator<Eigen::Vector3d> > JointStateDataStampedDeque;
+typedef std::deque<JointStateData, EV3dAllocator> JointStateDataDeque;
+typedef std::deque<JointStateDataStamped, EV3dAllocator> JointStateDataStampedDeque;
 
 
-typedef std::vector<ExtendedJointStateData,
-Eigen::aligned_allocator<Eigen::Vector3d> > ExtendedJointStateCollection;
-typedef std::vector<ExtendedJointStateDataStamped,
-Eigen::aligned_allocator<Eigen::Vector3d> > ExtendedJointStateStampedCollection;
+typedef std::vector<ExtendedJointStateData, EV3dAllocator> ExtendedJointStateCollection;
+typedef std::vector<ExtendedJointStateDataStamped, EV3dAllocator> ExtendedJointStateStampedCollection;
 
-typedef std::deque<ExtendedJointStateData,
-Eigen::aligned_allocator<Eigen::Vector3d> > ExtendedJointStateDeque;
-typedef std::deque<ExtendedJointStateDataStamped,
-Eigen::aligned_allocator<Eigen::Vector3d> > ExtendedJointStateStampedDeque;
+typedef std::deque<ExtendedJointStateData, EV3dAllocator> ExtendedJointStateDeque;
+typedef std::deque<ExtendedJointStateDataStamped, EV3dAllocator> ExtendedJointStateStampedDeque;
 
 typedef std::vector<JointData> JointDataCollection;
 typedef std::vector<JointDataStamped> JointDataStampedCollection;
